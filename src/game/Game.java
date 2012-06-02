3e0a85ef145e668cc12b47c3a7113d3acbb570b5
Dino <srdfxa@gmail.com>
2012-06-02 23:26:32 +0200

    Player2 + player movement capability

--- src/game/Game.java
+++ src/game/Game.java
@@ -15,46 +15,46 @@ import java.util.TimerTask;
 
 
 public class Game extends JPanel {
-	
-		
-	// animation constants and objects
-	private static final int FRAMES_PER_SECOND = 30;
-	private static final int MS_TO_WAIT = 1000 / FRAMES_PER_SECOND;
-		
-	
-	private Timer animationTimer = new Timer("Ball Animation");
-	private TimerTask animationTask = new AnimationUpdater();
-	
-	
-	public void start() {
+
+
+// animation constants and objects
+private static final int FRAMES_PER_SECOND = 60;
+private static final int MS_TO_WAIT = 1000 / FRAMES_PER_SECOND;
+
+
+private Timer animationTimer = new Timer("Ball Animation");
+private TimerTask animationTask = new AnimationUpdater();
+
+
+public void start() {
         animationTimer.schedule(animationTask, 0, MS_TO_WAIT);
     }
-	
-	
-	private class AnimationUpdater extends TimerTask {
+
+
+private class AnimationUpdater extends TimerTask {
 
         public void run() {
-        	
-        	ball.moveBall();
-        	      	
-        	//walls dimensions
+        
+         ball.moveBall();
+        
+         //walls dimensions
             int minY = ball.getHeight() / 2;
             int maxY = getHeight() - (ball.getHeight() / 2);
             
             int minX = 0 + ball.getWidth() / 2;
             int maxX = getWidth() - (ball.getWidth() / 2);
             
-         // check collisions with the bricks
-        	int ballX = (int) ball.getX();
-        	int ballY = (int) ball.getY();
+            //check collisions variables
+         int ballX = (int) ball.getX();
+         int ballY = (int) ball.getY();
 
-        	int ballminX = (int) ball.getX() - (ball.getWidth() / 2);
-        	int ballmaxX = (int) ball.getX() + (ball.getWidth() / 2);
-        	
-        	int ballminY = (int) ball.getY() + (ball.getHeight() / 2);
-        	int ballmaxY = (int) ball.getY() - (ball.getHeight() / 2);
+         int ballminX = (int) ball.getX() - (ball.getWidth() / 2);
+         int ballmaxX = (int) ball.getX() + (ball.getWidth() / 2);
+        
+         int ballminY = (int) ball.getY() + (ball.getHeight() / 2);
+         int ballmaxY = (int) ball.getY() - (ball.getHeight() / 2);
 
-            //check if the ball is out of bounds
+            //check if the ball is out of bounds (wall collisions)
             if (ball.getY() > maxY) {
                 ball.setY(maxY);
                 ball.setVY(-ball.getVY());
@@ -75,48 +75,48 @@ public class Game extends JPanel {
             
             //check collision with player one
             if( playerOne.contains(ballX, ballminY) || playerOne.contains(ballX, ballmaxY) ||
-            		playerOne.contains(ballminX, ballY) || playerOne.contains(ballmaxX, ballY) ) 
-            	{
-            		ball.setY(playerOne.getY() - ball.getWidth()/2);
-            		ball.setVY(-ball.getVY());
-            		
-            		ball.setX(playerOne.getX() - ball.getWidth()/2);
-            		ball.setVX(-ball.getVX());
-
-            		// beep after every paddle hit
-            		Toolkit.getDefaultToolkit().beep();
-            	}
+             playerOne.contains(ballminX, ballY) || playerOne.contains(ballmaxX, ballY) )
+             {
+             ball.setY(playerOne.getY() - ball.getWidth()/2);
+             ball.setVY(-ball.getVY());
             
-          //check collision with player two
-            if( playerTwo.contains(ballX, ballminY) || playerTwo.contains(ballX, ballmaxY) ||
-            		playerTwo.contains(ballminX, ballY) || playerTwo.contains(ballmaxX, ballY) ) 
-            	{
-            		ball.setY(playerTwo.getY() - ball.getWidth()/2);
-            		ball.setVY(-ball.getVY());
-            		
-            		ball.setX(playerTwo.getX() - ball.getWidth()/2);
-            		ball.setVX(-ball.getVX());
+             ball.setX(playerOne.getX() - ball.getWidth()/2);
+             ball.setVX(-ball.getVX());
 
-            		// beep after every paddle hit
-            		Toolkit.getDefaultToolkit().beep();
-            	}
+             // beep after every paddle hit
+             Toolkit.getDefaultToolkit().beep();
+             }
+            
+            //check collision with player two
+            if( playerTwo.contains(ballX, ballminY) || playerTwo.contains(ballX, ballmaxY) ||
+             playerTwo.contains(ballminX, ballY) || playerTwo.contains(ballmaxX, ballY) )
+             {
+             ball.setY(playerTwo.getY() - ball.getWidth()/2);
+             ball.setVY(-ball.getVY());
+            
+             ball.setX(playerTwo.getX() - ball.getWidth()/2);
+             ball.setVX(-ball.getVX());
+
+             // beep after every paddle hit
+             Toolkit.getDefaultToolkit().beep();
+             }
 
             repaint();
         }
 
     }
-	
-	
-	
-	
-	
-	//ball constants and object
-    private static final int INITIAL_Y_VELOCITY = 4;
+
+
+
+
+
+//ball constants and object
+    private static final int INITIAL_Y_VELOCITY = 1;
     private static final int INITIAL_X_VELOCITY = 15;
-    private static final double ACCELERATION = 0.2;
-    private Ball ball = new Ball(200, 0, INITIAL_X_VELOCITY, INITIAL_Y_VELOCITY, 0, ACCELERATION, 7, 7);
-	
-	
+    private static final double ACCELERATION = 0;
+    private Ball ball = new Ball(200, 0, INITIAL_X_VELOCITY, INITIAL_Y_VELOCITY, 0, ACCELERATION, 10, 10);
+
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
               
@@ -130,78 +130,110 @@ public class Game extends JPanel {
         playerOne.paint(g);
         playerTwo.paint(g);
        
-	}
+}
     
     
     
     
     
     //player one and two
-	private static final int PlAYER_VY = 8;
-	private static final int PlAYERONE_X = 10;
-	private static final int PlAYERONE_Y = 250;
-	private static final int PlAYERTWO_X = 1170;
-	private static final int PlAYERTWO_Y = 250;
-	
-	private Player playerOne = new Player(PlAYERONE_X, PlAYERONE_Y, 0, 0);
-	private Player playerTwo = new Player(PlAYERTWO_X, PlAYERTWO_Y, 0, 0);
-	
-	
-	
-	
-	
-	//key listener
-    private class GameKeysListener implements KeyListener {
-    	
-		@Override
-		public void keyPressed(KeyEvent e) {
+private static final int PlAYER_VY = 40;
+private static final int PlAYERONE_X = 40;
+private static final int PlAYERONE_Y = 250;
+private static final int PlAYERTWO_X = 1130;
+private static final int PlAYERTWO_Y = 250;
 
-            if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            	playerOne.setVY(-PlAYER_VY);
+private Player playerOne = new Player(PlAYERONE_X, PlAYERONE_Y, 0, 0);
+private Player playerTwo = new Player(PlAYERTWO_X, PlAYERTWO_Y, 0, 0);
+
+
+
+
+
+//key listener
+    private class GameKeysListener implements KeyListener {
+    
+     //move player one
+@Override
+public void keyPressed(KeyEvent e) {
+
+            if (e.getKeyCode() == KeyEvent.VK_UP) {
+             playerOne.setVY(-PlAYER_VY);
+             playerOne.movePlayer();
             }
-            else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            	playerOne.setVY(PlAYER_VY);
+            else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+             playerOne.setVY(PlAYER_VY);
+             playerOne.movePlayer();
             }
             else if (e.getKeyCode() == KeyEvent.VK_R) {
-            	playerOne.setX(PlAYERONE_X);
-            	playerOne.setY(PlAYERONE_Y);
-            	playerOne.setVY(0);
+             playerOne.setX(PlAYERONE_X);
+             playerOne.setY(PlAYERONE_Y);
+             playerOne.setVY(0);
+             playerOne.movePlayer();
             }
             
-            playerOne.movePlayer();
-		}
+            
+            
+            
+            if (e.getKeyCode() == KeyEvent.VK_W) {
+             playerTwo.setVY(-PlAYER_VY);
+             playerTwo.movePlayer();
+            }
+            else if (e.getKeyCode() == KeyEvent.VK_S) {
+             playerTwo.setVY(PlAYER_VY);
+             playerTwo.movePlayer();
+            }
+            else if (e.getKeyCode() == KeyEvent.VK_R) {
+             playerTwo.setX(PlAYERONE_X);
+             playerTwo.setY(PlAYERONE_Y);
+             playerTwo.setVY(0);
+             playerTwo.movePlayer();
+            }
+            
+            
+}
 
-		@Override
+
+@Override
         public void keyReleased(KeyEvent e) {
-			playerOne.setVY(0);
+playerOne.setVY(0);
         }
 
-		@Override
-		public void keyTyped(KeyEvent e) {
-			// empty method, do not need to respond to this event
-		}
+@Override
+public void keyTyped(KeyEvent e) {
+// empty method, do not need to respond to this event
+}
     }
-	
 
 
-	
-	
-	
-	
-	public static void main(String[] args) {
-		
-		JFrame window = new JFrame();
+    public Game() {
+     super();
+    
+     // setup game keys listener
+     setFocusable(true);
+     addKeyListener(new GameKeysListener());
+    }
+
+
+
+
+
+public static void main(String[] args) {
+
+JFrame window = new JFrame();
         
         Game panel = new Game();
         
         window.add(panel);
         window.setSize(1200, 600);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBackground(Color.BLACK);
+        window.setResizable(false);
         window.setVisible(true);
         
         panel.start();
 
-	}
-	
-	
+}
+
+
 }
--- src/game/Player.java
+++ src/game/Player.java
@@ -6,135 +6,137 @@ import java.awt.Graphics;
 
 
 public class Player {
-	
-	//position of the player
-	private int x;
-	private int y;
-	
-	//velocity of the player
-	private int vx;
-	private int vy;
-	
-	//player color
-	private Color color = Color.BLUE;
-	
-	//player dimensions
-	private int width = 10;
-	private int height = 60;
-	
-	
-	
-	
-	
-	//default constructor
-	public Player(int x, int y, int vx, int vy, int width, int height) {
-		this.x = x;
-		this.y = y;
-		this.vx = vx;
-		this.vy = vy;
-		this.width = width;
-		this.height = height;
-	}
-	
-	//constructor with no dimension values
-	public Player(int x, int y, int vx, int vy) {
-		this.x = x;
-		this.y = y;
-		this.vx = vx;
-		this.vy = vy;
-	}
 
-	
-	
-	
-	
-	//update player's position
-	public void movePlayer() {
-		this.x += vx;
-		this.y += vy;
-	}
-	
-	
-	
-	
-		
-	//paint player
-	public void paint(Graphics g) {
-		g.setColor(color);
-		g.fillRect(x, y, width, height);
-	}
+//position of the player
+private int x;
+private int y;
 
-	
-	
-	
-	public boolean contains(int x, int y) {
-		int offsetX = x - getX();
-		int offsetY = y - getY();
-		
-		if( offsetX >= 0 && offsetX <= width && offsetY >= 0 && offsetY <= height ) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	
-	
-	
-	
-	//getters
-	public int getX() {
-		return x;
-	}
+//velocity of the player (when key is pressed)
+private int vx;
+private int vy;
 
-	public int getY() {
-		return y;
-	}
+//player color
+private Color color = Color.BLUE;
 
-	public int getVX() {
-		return vx;
-	}
+//player dimensions
+private int width = 10;
+private int height = 100;
 
-	public int getVY() {
-		return vy;
-	}
-	
-	public int getWidth() {
-		return width;
-	}
 
-	public int getHeight() {
-		return height;
-	}
-	
-	
-	
-	
-	
-	//setters
-	public void setX(int x) {
-		this.x = x;
-	}
 
-	public void setY(int y) {
-		this.y = y;
-	}
 
-	public void setVX(int vx) {
-		this.vx = vx;
-	}
-	
-	public void setVY(int vy) {
-		this.vy = vy;
-	}
-	
-	public void setWidth(int width) {
-		this.width = width;
-	}
 
-	public void setHeight(int height) {
-		this.height = height;
-	}
-	
-	
+//default constructor
+public Player(int x, int y, int vx, int vy, int width, int height) {
+this.x = x;
+this.y = y;
+this.vx = vx;
+this.vy = vy;
+this.width = width;
+this.height = height;
+}
+
+//constructor with no dimension values
+public Player(int x, int y, int vx, int vy) {
+this.x = x;
+this.y = y;
+this.vx = vx;
+this.vy = vy;
+}
+
+
+
+
+
+//update player's position
+public void movePlayer() {
+this.x += vx;
+this.y += vy;
+}
+
+
+
+
+
+//paint player
+public void paint(Graphics g) {
+g.setColor(color);
+g.fillRect(x, y, width, height);
+}
+
+
+
+
+
+public boolean contains(int x, int y) {
+int offsetX = x - getX();
+int offsetY = y - getY();
+
+if( offsetX >= 0 && offsetX <= width && offsetY >= 0 && offsetY <= height ) {
+return true;
+}
+else {
+return false;
+}
+}
+
+
+
+
+
+//getters
+public int getX() {
+return x;
+}
+
+public int getY() {
+return y;
+}
+
+public int getVX() {
+return vx;
+}
+
+public int getVY() {
+return vy;
+}
+
+public int getWidth() {
+return width;
+}
+
+public int getHeight() {
+return height;
+}
+
+
+
+
+
+//setters
+public void setX(int x) {
+this.x = x;
+}
+
+public void setY(int y) {
+this.y = y;
+}
+
+public void setVX(int vx) {
+this.vx = vx;
+}
+
+public void setVY(int vy) {
+this.vy = vy;
+}
+
+public void setWidth(int width) {
+this.width = width;
+}
+
+public void setHeight(int height) {
+this.height = height;
+}
+
+
 }
